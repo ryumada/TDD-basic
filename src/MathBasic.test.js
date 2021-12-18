d229802a626efddd50b494b5695371df2093e347
ryumada <rizukiryumada@gmail.com>
2021-12-18 20:39:38 +0700

    created MathBasic with its test scenario

---
+++ src/MathBasic.test.js
@@ -0,0 +1,98 @@
+const MathBasic = require('./MathBasic')
+
+describe('A MathBasic', () => {
+  it('should contains add, subtract, multiply, and divide function', () => {
+    expect(MathBasic).toHaveProperty('add')
+    expect(MathBasic).toHaveProperty('substract')
+    expect(MathBasic).toHaveProperty('multiply')
+    expect(MathBasic).toHaveProperty('divide')
+    expect(MathBasic.add).toBeInstanceOf(Function)
+    expect(MathBasic.substract).toBeInstanceOf(Function)
+    expect(MathBasic.multiply).toBeInstanceOf(Function)
+    expect(MathBasic.divide).toBeInstanceOf(Function)
+  })
+
+  describe('An add function', () => {
+    it('should throw error when not given 2 parameters', () => {
+      expect(() => MathBasic.add()).toThrowError()
+      expect(() => MathBasic.add(1)).toThrowError()
+      expect(() => MathBasic.add(1, 2, 3)).toThrowError()
+      expect(() => MathBasic.add(1, 2, 3, 4)).toThrowError()
+    })
+
+    it('should throw error when given non-number parameters', () => {
+      expect(() => MathBasic.add('1', '2')).toThrowError()
+      expect(() => MathBasic.add(true, {})).toThrowError()
+      expect(() => MathBasic.add(null, false)).toThrowError()
+    })
+
+    it('should return a + b when given two number parameters', () => {
+      expect(MathBasic.add(2, 2)).toEqual(4)
+      expect(MathBasic.add(16, 8)).toEqual(24)
+      expect(MathBasic.add(3, 7)).toEqual(10)
+    })
+  })
+
+  describe('A substract function', () => {
+    it('should throw error when not given 2 parameters', () => {
+      expect(() => MathBasic.substract()).toThrowError()
+      expect(() => MathBasic.substract(1)).toThrowError()
+      expect(() => MathBasic.substract(1, 2, 3)).toThrowError()
+      expect(() => MathBasic.substract(1, 2, 3, 4)).toThrowError()
+    })
+
+    it('should throw error when given non-number parameters', () => {
+      expect(() => MathBasic.substract('1', '2')).toThrowError()
+      expect(() => MathBasic.substract(true, {})).toThrowError()
+      expect(() => MathBasic.substract(null, false)).toThrowError()
+    })
+
+    it('should return a - b when given two number parameters', () => {
+      expect(MathBasic.substract(2, 2)).toEqual(0)
+      expect(MathBasic.substract(27, 17)).toEqual(10)
+      expect(MathBasic.substract(1, 2)).toEqual(-1)
+    })
+  })
+
+  describe('A multiply function', () => {
+    it('should throw error when not given 2 parameters', () => {
+      expect(() => MathBasic.multiply()).toThrowError()
+      expect(() => MathBasic.multiply(1)).toThrowError()
+      expect(() => MathBasic.multiply(1, 2, 3)).toThrowError()
+      expect(() => MathBasic.multiply(1, 2, 3, 4)).toThrowError()
+    })
+
+    it('should throw error when given non-number parameters', () => {
+      expect(() => MathBasic.multiply('1', '2')).toThrowError()
+      expect(() => MathBasic.multiply(true, {})).toThrowError()
+      expect(() => MathBasic.multiply(null, false)).toThrowError()
+    })
+
+    it('should return a * b when given two number parameters', () => {
+      expect(MathBasic.multiply(1, 2)).toEqual(2)
+      expect(MathBasic.multiply(10, 10)).toEqual(100)
+      expect(MathBasic.multiply(2, 4)).toEqual(8)
+    })
+  })
+
+  describe('A divide function', () => {
+    it('should throw error when not given 2 parameters', () => {
+      expect(() => MathBasic.divide()).toThrowError()
+      expect(() => MathBasic.divide(1)).toThrowError()
+      expect(() => MathBasic.divide(1, 2, 3)).toThrowError()
+      expect(() => MathBasic.divide(1, 2, 3, 4)).toThrowError()
+    })
+
+    it('should throw error when given non-number parameters', () => {
+      expect(() => MathBasic.divide('1', '2')).toThrowError()
+      expect(() => MathBasic.divide(true, {})).toThrowError()
+      expect(() => MathBasic.divide(null, false)).toThrowError()
+    })
+
+    it('should return a / b when given two number parameters', () => {
+      expect(MathBasic.divide(2, 2)).toEqual(1)
+      expect(MathBasic.divide(100, 10)).toEqual(10)
+      expect(MathBasic.divide(50, 2)).toEqual(25)
+    })
+  })
+})
