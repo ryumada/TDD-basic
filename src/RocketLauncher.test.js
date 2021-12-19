511da752035d04869554f59ba2d0c844947fe3c4
ryumada <rizukiryumada@gmail.com>
2021-12-19 07:10:50 +0700

    created stub testing

--- src/RocketLauncher.test.js
+++ src/RocketLauncher.test.js
@@ -33,4 +33,26 @@ describe('A RocketLauncher', () => {
     expect(spaceXRocket.engineStatus).toEqual('inactive')
     expect(rocketLauncher.rockets.length).toEqual(1)
   })
+
+  // stub example
+  it('should return correct result when repair kit cannot repair the rocket', async () => {
+    // Arrange
+    /**
+     * Stub?! butuh implementasi fungsi untuk menghasilkan keadaan sesuai skenario uji
+     * naum kita tidak menguji apapun terkait fungsi yang diubah
+     * membuat error boongan buat dicek apakah bakal menghasilkan return yang sesuai
+     */
+
+    const fakeRocketRepairKit = {
+      repair: () => Promise.reject(new Error('failed to repair the rocket'))
+    }
+
+    const rocketLauncher = new RocketLauncher(fakeRocketRepairKit, [{}])
+
+    // Action
+    const result = await rocketLauncher.repairAllRockets()
+
+    // Assert
+    expect(result).toEqual('there was 1 of 1 rocket fail to repair')
+  })
 })
